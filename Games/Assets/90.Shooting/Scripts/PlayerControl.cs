5e451a3fe6f129c154126cdfb5df299bdb9f4bfa
seyoungChu <seyoung@aceproject.co.kr>
2018-06-21 22:50:27 +0900

    6_21_lecture

---
+++ Games/Assets/90.Shooting/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 1.0f;
+    private Transform myTransform = null;
+    public GameObject myBullet = null;
+	// Use this for initialization
+	void Start () {
+        myTransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveAmount = Speed * Vector3.left * Input.GetAxis("Horizontal") * Time.deltaTime;
+        myTransform.Translate(moveAmount);
+
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            Instantiate(myBullet, myTransform.position, Quaternion.identity);
+        }
+	}
+}
