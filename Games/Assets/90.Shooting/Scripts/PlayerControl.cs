9d5a314c06e4a8f479037051cddf212e5fe812ec
keori <wjdrmsl@gmail.com>
2018-06-28 21:58:32 +0900

    4주차 슈팅 게임

---
+++ Games/Assets/90.Shooting/Scripts/PlayerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 5.0f;
+    public GameObject myBullet = null;
+    private Transform myTransform = null;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = GetComponent<Transform>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveAmount = Speed * Vector3.left * Input.GetAxis ("Horizontal") * Time.deltaTime;
+        myTransform.Translate(moveAmount);
+
+        if (Input.GetKeyDown(KeyCode.Space) == true) {
+            // 총알 생성
+            Instantiate(myBullet, myTransform.position, Quaternion.identity);
+        }
+	}
+}
---
+++ Games/Assets/90.Shooting/Scripts/VictoryControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class VictoryControl : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        float CenterX = Screen.width / 2;
+        float CenterY = Screen.height / 2;
+
+        Rect ButtonRect = new Rect(CenterX - 50.0f, CenterY - 50.0f, 100.0f, 100.0f);
+        if (GUI.Button(ButtonRect, "You Win!") == true) {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
