646c01a4a8067587b4659871af877d54b82bcfd0
seyoungChu <seyoung@aceproject.co.kr>
2018-07-05 22:13:04 +0900

    180705. lecture

---
+++ Games/Assets/91.Defense/Scripts/BeetleControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeetleControl : MonoBehaviour {
+
+    public float Speed = 1.0f;
+    private Transform myTransform = null;
+    public Vector3[] TargetPositions = new Vector3[10];
+    private int AddIndex = 0;
+    private int CurrentIndex = 0;
+    
+	// Use this for initialization
+	void Start () {
+        myTransform = GetComponent<Transform>(); //cashing.
+        //애니메이션 재생모드를 반복 모드로 세팅.
+        GetComponent<Animation>().wrapMode = WrapMode.Loop;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //방향+크기 벡터 = 목표 위치 - 내 위치.
+        Vector3 Diff = TargetPositions[CurrentIndex] - myTransform.position;
+        //거리를 구하는 함수 Distance (목표위치, 내위치)
+        float Distance = Vector3.Distance(TargetPositions[CurrentIndex],
+                                            myTransform.position);
+        if (Distance < 0.1f) //가깝다면.
+        {
+            CurrentIndex++;//CurrentIndex를 1 증가합니다.
+            //다음 목표 위치가 0,0,0이라면.
+            if(TargetPositions[CurrentIndex] == Vector3.zero)
+            {
+                Destroy(gameObject);
+            }
+        }
+        //방향 벡터 = 방향+크기 벡터 - 크기.
+        Vector3 Direction = Diff.normalized;
+
+        //이동
+        myTransform.Translate(Speed * Direction * Time.deltaTime, Space.World);
+        myTransform.LookAt(TargetPositions[CurrentIndex]);
+	}
+    /// <summary>
+    /// 비틀의 목표 위치들을 저장하는 함수.
+    /// </summary>
+    /// <param name="targetPos"></param>
+    void SetTargetPosition(Vector3 targetPos)
+    {
+        TargetPositions[AddIndex] = targetPos;
+        AddIndex++; //Addindex = AddIndex + 1;
+    }
+}
--- Games/Assets/91.Defense/Scripts/SpawnersControl.cs
+++ Games/Assets/91.Defense/Scripts/SpawnersControl.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class SpawnersControl : MonoBehaviour {
 
     public GameObject[] Spawners = null;
+    public GameObject SpawnObject = null;
 
+    public float SpawnDelay = 1.0f;
+    private float LastSpawnTime = 0.0f;
     // Use this for initialization
     void Start() {
 
@@ -13,7 +16,19 @@ public class SpawnersControl : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        //현재시간이 마지막으로 스폰한 시간으로부터 딜레이 시간만큼 지났으면.
+        if(Time.time > LastSpawnTime + SpawnDelay)
+        {
+            LastSpawnTime = Time.time;//이벤트 발생시간 현재시간으로 세팅.
+            GameObject Beetle = (GameObject)Instantiate(SpawnObject,
+                Spawners[0].transform.position,
+                Spawners[0].transform.rotation);
+            for(int i = 1; i < Spawners.Length; i++)
+            {
+                Beetle.SendMessage("SetTargetPosition", Spawners[i].transform.position);
+            }
 
+        }
     }
     /// <summary>
     /// 선택되었을때만 기즈모를 그리는 이벤트 함수.
@@ -29,21 +44,23 @@ public class SpawnersControl : MonoBehaviour {
         {
             return;
         }
-        Gizmos.color = Color.red;
-        for(int i = 0; i < Spawners.Length; i++)//0,1,2,3,4,5
-        {
-            //vector3.one = (1.0f,1.0f,1.0f)
-            Gizmos.DrawWireCube(Spawners[i].transform.position, Vector3.one);
-        }
-        Gizmos.color = Color.blue;
+        
         for(int i = 0; i < Spawners.Length; i++) //0,1,2,3,4,5
         {
-            Gizmos.DrawWireSphere(Spawners[i].transform.position, 1.0f);
-            if( i < Spawners.Length - 1) // 0,1,2,3,4
-            {   //[0,1] ,[1,2], [2,3] , [3,4], [4,5]
-                Gizmos.DrawLine(Spawners[i].transform.position,
-                    Spawners[i + 1].transform.position);
+            if(Spawners[i] != null)
+            {
+                Gizmos.color = Color.red;
+                //vector3.one = (1.0f,1.0f,1.0f)
+                Gizmos.DrawWireCube(Spawners[i].transform.position, Vector3.one);
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(Spawners[i].transform.position, 1.0f);
+                if (i < Spawners.Length - 1 && Spawners[i+1] != null) // 0,1,2,3,4
+                {   //[0,1] ,[1,2], [2,3] , [3,4], [4,5]
+                    Gizmos.DrawLine(Spawners[i].transform.position,
+                        Spawners[i + 1].transform.position);
+                }
             }
+            
         }
     }
     
