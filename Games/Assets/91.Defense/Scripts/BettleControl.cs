0890cba5ec1fb3f9212814b728fcb78d33f124a6
이상희 <>
2018-07-10 20:37:55 +0900

    7월 10일 수업 전

---
+++ Games/Assets/91.Defense/Scripts/BettleControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BettleControl : MonoBehaviour {
+
+    public float Speed = 1.0f;
+    private Transform myTransform = null;
+    public Vector3[] TargetPositions = new Vector3[10];
+    private int AddIndex = 0;
+    private int CurrentIndex = 0; 
+
+	// Use this for initialization
+	void Start () {
+        myTransform = GetComponent<Transform>(); //cashing
+        GetComponent<Animation>().wrapMode = WrapMode.Loop;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //가고자하는 위치 - 현재 위치 = 이동할 크기와 방향
+        Vector3 Diff = TargetPositions[CurrentIndex] - myTransform.position;
+        float Distance = Vector3.Distance(TargetPositions[CurrentIndex], myTransform.position);
+
+        if(Distance < 0.1f) // 가깝다면
+        {
+            CurrentIndex++; // CurrentIndex 1 증가시킴
+            if(TargetPositions[CurrentIndex] == Vector3.zero)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        Vector3 Direction = Diff.normalized; //정규화 방향 벡터 = 방행 + 크기 벡터 - 크기
+
+        //이동
+        myTransform.Translate(Speed * Direction * Time.deltaTime, Space.World);
+        myTransform.LookAt(TargetPositions[CurrentIndex]); // 가고자하는 위치로 오브젝트 돌려줌
+	}
+
+    //비틀의 목표 위치들을 저장하는 함수
+    void SetTargetPosition(Vector3 taregetPos) {
+        TargetPositions[AddIndex] = taregetPos;
+        AddIndex++;
+    }
+}
---
+++ Games/Assets/91.Defense/Scripts/SpawnersControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnersControl : MonoBehaviour {
+
+    public GameObject[] Spawners = null;
+    public GameObject SpawnObject = null;
+
+    public float SpawnDelay = 1.0f; // 이벤트 발생 간격
+    private float LastSpawenTime = 0.0f; // 마지막 이벤트 발생 시간
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //현재 시간이 마지막으로 스폰한 시간으로부터 딜레이 시간이 지났으면
+        if(Time.time > LastSpawenTime + SpawnDelay)
+        {
+            LastSpawenTime = Time.time; // 이벤트 발생시간을 현재 시간으로 셋팅, 할당 
+            GameObject Beetle = (GameObject)Instantiate(SpawnObject, Spawners[0].transform.position, Spawners[0].transform.rotation);
+
+            for (int i = 0; i < Spawners.Length; i++ ) {
+                Beetle.SendMessage("SetTargetPosition", Spawners[i].transform.position);
+            }
+        }
+	}
+
+    // 해당 게임 오브젝트를 선택했을 때에만 기즈모를 보여주는 이벤트 함수
+    private void OnDrawGizmosSelected()
+    {
+        // 지름 1.0f 구의 기즈모를 그려라!!!
+        Gizmos.DrawSphere(transform.position, 1.0f);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if(Spawners == null || Spawners.Length == 0) 
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        for (int i = 0; i < Spawners.Length; i++) 
+        {
+            Gizmos.DrawWireCube(Spawners[i].transform.position, Vector3.one);
+        }
+
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < Spawners.Length; i++)
+        {
+            Gizmos.DrawWireSphere(Spawners[i].transform.position, 1.0f);
+            if (i < Spawners.Length - 1) 
+            {
+                Gizmos.DrawLine(Spawners[i].transform.position, Spawners[i + 1].transform.position);
+            }
+        }
+
+    }
+}
