c274420a94b238203e674a3b1fad12eb86cb1f4f
kite007 <kite007@gmail.com>
2018-07-10 20:34:40 +0900

    no message

---
+++ Games/Assets/90.Shooting/Scripts/MainControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControl : MonoBehaviour {
+
+    static public int Score = 0;
+    static public int Life = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //해상도 1024x768 기준으로 라벨을 출력함.
+    private void OnGUI()
+    {
+        Rect labelRect1 = new Rect(10.0f, 10.0f, 100.0f, 30.0f);
+        GUI.Label(labelRect1, "Score : " + MainControl.Score);
+        Rect labelRect2 = new Rect(10.0f, 40.0f, 100.0f, 30.0f);
+        GUI.Label(labelRect2, "Life : " + MainControl.Life);
+
+    }
+}
