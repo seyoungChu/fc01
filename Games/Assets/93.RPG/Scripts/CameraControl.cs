f87622cc2f9fd57c1bfdc149e5d5d254bd678d53
seyoungChu <seyoung@aceproject.co.kr>
2018-08-09 10:44:22 +0900

    180809_lecture

---
+++ Games/Assets/93.RPG/Scripts/CameraControl.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    //1인칭.2인칭.3인칭 모드
+    public enum CameraViewpointState { FIRST, SECOND, THIRD }
+    //기본적으로 3인칭카메라.
+    public CameraViewpointState cameraState = CameraViewpointState.THIRD;
+
+    private Transform myTransform = null;
+    //3인칭 카메라 관련 멤버 변수.
+    //카메라와 캐릭터간의 거리.
+    public float Distance = 5.0f;
+    //카메라와 캐릭터로부터의 높이.
+    public float Height = 1.5f;
+    //댐핑용 변수들.
+    public float HeightDamping = 2.0f;
+    public float RotationDamping = 3.0f;
+    //카메라가 바라보게될 타겟 게임오브젝트.
+    public GameObject Target = null;
+
+    //2인칭 관련.(모델 회전뷰)
+    public float RotateSpeed = 10.0f;
+
+    //1인칭 관련
+    public enum RotationAxes { MouseXandY , MouseX, MouseY }
+    public RotationAxes axes = RotationAxes.MouseXandY;
+    public float sensitivityX = 5.0f;
+    public float sensitivityY = 5.0f;
+    public float minimumX = 0.0f;
+    public float maximumX = 90.0f;
+    public float minimumY = -30.0f;
+    public float maximumY = 30.0f;
+
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+    public Transform ViewPoint = null;
+
+
+	// Use this for initialization
+	void Start () {
+        myTransform = GetComponent<Transform>();
+	}
+    //3인칭 뷰.
+    void ThirdView()
+    {
+        if(Target != null)
+        {   //타겟 트랜스폼.
+            Transform targetTrans = Target.transform;
+            //목표로하는 y축 회전값과 높이값.
+            float wantedRotationAngle = targetTrans.eulerAngles.y;
+            float wantedHeight = targetTrans.position.y + Height;
+            //현재 나의 y축 회전값과 높이값.
+            float currentRotationAngle = myTransform.eulerAngles.y;
+            float currentHeight = myTransform.position.y;
+            //현재 회전값을 보간해서 목표로하는 회전값에 가깝도록.
+            currentRotationAngle = Mathf.LerpAngle(currentRotationAngle,
+                wantedRotationAngle,
+                RotationDamping * Time.deltaTime);
+            //현재 높이값을 보간해서 목표로 하는 높이값에 가깝도록.
+            currentHeight = Mathf.Lerp(currentHeight,
+                wantedHeight,
+                HeightDamping * Time.deltaTime);
+            //회전 벡터를 만들기위해 사원수를 하나 만듭니다.
+            //ii+ji+ki+a = 0 -> quaternion 3차원 공간의 위치와 각도를 표시하는 수 체계
+            Quaternion currentRotation = Quaternion.Euler(0.0f, currentRotationAngle,
+                0.0f);
+
+            //카메라의 3인칭 이동.
+            //카메라를 먼저 타겟의 위치로 이동
+            myTransform.position = targetTrans.position;
+            //카메라가 바라보아야할 방향으로 회전후 원하는 거리만큼 빼줍니다.
+            myTransform.position -= currentRotation * Vector3.forward * Distance;
+            //카메라의 높이를 지정합니다.
+            myTransform.position = new Vector3(myTransform.position.x,
+                                                currentHeight,
+                                                myTransform.position.z);
+            //카메라가 타겟을 바라보게 됩니다.
+            myTransform.LookAt(targetTrans);
+        }
+    }
+    //2인칭 뷰
+    void SecondView()
+    {   //카메라를 타겟위치를 중심으로 y축방향으로 RotateSpeed만큼 회전합니다.
+        myTransform.RotateAround(Target.transform.position, Vector3.up,
+            Time.deltaTime * RotateSpeed);
+        //회전하면서 타겟을 바라보게 됩니다.
+        myTransform.LookAt(Target.transform);
+    }
+    //1인칭 뷰
+    void FirstView()
+    {
+        //마우스의 좌우이동값을 기준으로 y축 회전 값을 구하고.
+        rotationX = myTransform.localEulerAngles.y +
+            Input.GetAxis("Mouse X") * sensitivityX;
+        //y축 회전값은 최소,최대값사이의 값으로 하고.
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        //마우스의 상하이동값을 기준으로 현재 x축 값에 더해줍니다.
+        rotationY = rotationY + Input.GetAxis("Mouse Y") * sensitivityY;
+        //x축 회전값은 최소,최대값 사이 값으로 합니다.
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        //마우스 좌우상하로 시선을 조종한다면.
+        if(axes == RotationAxes.MouseXandY)
+        {
+            myTransform.localEulerAngles = new Vector3(
+                -rotationY, rotationX, 0.0f);
+        }else if(axes == RotationAxes.MouseX)
+        {   //마우스의 좌우로 시선을 조종한다면.
+            myTransform.localEulerAngles = new Vector3(0.0f, rotationX, 0.0f);
+        }else if(axes == RotationAxes.MouseY)
+        {   //마우스의 상하로 시선을 조종한다면.
+            myTransform.localEulerAngles = new Vector3(-rotationY,
+                myTransform.localEulerAngles.y, 0.0f);
+        }
+        myTransform.position = ViewPoint.position;
+    }
+	
+	// 모든 업데이트가 호출된 이후에 호출되는 업데이트.
+	void LateUpdate () {
+		switch(cameraState)
+        {
+            case CameraViewpointState.FIRST:
+                FirstView();
+                break;
+            case CameraViewpointState.SECOND:
+                SecondView();
+                break;
+            case CameraViewpointState.THIRD:
+                ThirdView();
+                break;
+        }
+	}
+}
---
+++ Games/Assets/93.RPG/Scripts/GoblinControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinControl : MonoBehaviour
+{
+    public enum GoblinState { None, Idle, Patrol, Wait, MoveToTarget, Attack, Damage ,Die}
+    public GoblinState myState = GoblinState.None;
+    //상태를 원래대로 돌려주기위해 이전 상태를 저장해 놓습니다.
+    public GoblinState lastState = GoblinState.None;
+
+    public float MoveSpeed = 1.0f;//이동 속도.
+    public float RotateSpeed = 100.0f;//회전 속도.
+    public GameObject TargetPlayer = null;//타겟 플레이어.
+    public Vector3 TargetPosition = Vector3.zero;//(0,0,0) 패트롤시 이동할 목표위치.
+
+    private Animation myAnimation = null;//캐싱.
+    private Transform myTransform = null;//캐싱.
+    //사용할 애니메이션.
+    public AnimationClip IdleAnimation = null;//대기 애니메이션
+    public AnimationClip MoveAnimation = null;//이동 애니메이션
+    public AnimationClip AttackAnimation = null;//공격 애니메이션
+    public AnimationClip DamageAnimation = null;//피격 애니메이션
+    public AnimationClip DieAnimation = null;//죽는 애니메이션.
+    //
+    public int HP = 100; //
+    public float AttackRange = 1.5f; //공격 거리
+    public GameObject DamageEffect = null; //피격 이펙트.
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //내 기본상태는 대기
+        myState = GoblinState.Idle;
+        myAnimation = GetComponent<Animation>();
+        myTransform = GetComponent<Transform>();
+        //애니메이션 세팅.
+        myAnimation[IdleAnimation.name].wrapMode = WrapMode.Loop;
+        myAnimation[MoveAnimation.name].wrapMode = WrapMode.Loop;
+        myAnimation[AttackAnimation.name].wrapMode = WrapMode.Once;
+        myAnimation[DamageAnimation.name].wrapMode = WrapMode.Once;
+        myAnimation[DamageAnimation.name].layer = 10; //더 중요하게 재생됩니다.
+        myAnimation[DieAnimation.name].wrapMode = WrapMode.Once;
+        myAnimation[DieAnimation.name].layer = 10;
+        //애니메이션 이벤트 세팅.
+        AddAnimationEvent("AttackAnimFinish", AttackAnimation);
+        AddAnimationEvent("DamageAnimFinish", DamageAnimation);
+        AddAnimationEvent("DieAnimFinish", DieAnimation);
+    }
+    #region 애니메이션이벤트
+    //애니메이션 클립에 애니메이션 이벤트를 추가합니다.
+    void AddAnimationEvent(string FuncName, AnimationClip clip)
+    {
+        AnimationEvent newEvent = new AnimationEvent();
+        newEvent.functionName = FuncName;
+        newEvent.time = clip.length - 0.1f;
+        clip.AddEvent(newEvent);
+    }
+    //공격 애니메이션이 끝나면 호출됩니다.
+    void AttackAnimFinish()
+    {
+    }
+    void DamageAnimFinish()
+    {
+    }
+    void DieAnimFinish()
+    {
+
+    }
+    #endregion
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
